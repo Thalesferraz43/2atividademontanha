f69290bcd75b8e9c49729db734a2df0a910208e3
Thales Ferraz <thalesferraz43@gmail.com>
2022-10-09 10:45:06 -0300

    atividademontanha2

---
+++ src/atividade2.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class atividade2 {
+ 
+ public static void main(String[] args) {
+
+    int num;
+    System.out.print("Digite um número: ");
+    Scanner sc = new Scanner(System.in); 
+    num = sc.nextInt();
+    System.out.println("O número digitado foi "+ num);
+ }
+}
+
